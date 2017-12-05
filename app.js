7f08d463d18d3ceea8a0f0f9c8e9b74b19053c72
k.oishi <ice9freeze@gmail.com>
2017-12-05 12:27:45 +0000

    逆順と順位表記

--- app.js
+++ app.js
@@ -25,11 +25,7 @@ rl.on('line',(lineString) => {
             value.popu15 += popu;
         }
         map.set(prefecture,value);
-        //console.log(year);
-        //console.log(prefecture);
-        //console.log(popu);
     }
-    //console.log(lineString);
 });
 rl.resume();
 rl.on('close',() => {
@@ -38,10 +34,10 @@ rl.on('close',() => {
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingString = rankingArray.map((pair) => {
-        return pair[0] + ':' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    const rankingString = rankingArray.map((pair,i) => {
+        return 'Rank:' + (i + 1) + ' ' + pair[0] + ':' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
    console.log(rankingString);
 });
